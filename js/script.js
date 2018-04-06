76e6f64b2fef04d6c0f4985b4895525cdd430973
Maciek Bulski <fragmast3r@gmail.com>
2018-04-06 19:35:52 +0100

    fixed vars in script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 var a = 3;
-	h = 5;
-	triangleArea = a * h / 2;
+var	h = 5;
+var	triangleArea = a * h / 2;
 console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
 prompt('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
